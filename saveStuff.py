96af967df8e687a2f252fd4c2f414d9b1d4fafc7
Jessie <jessiexxcheung@gmail.com>
2019-07-11 17:03:13 -0400

    Added spectro and peak to main

--- saveStuff.py
+++ saveStuff.py
@@ -3,8 +3,16 @@ import numpy as np
 import librosa
 import matplotlib.pyplot as plt
 from IPython.display import Audio
-import spectrogram
-import find_peaks
 %matplotlib notebook
 import matplotlib.mlab as mlab
 from microphone import record_audio
+
+# our own files
+import spectrogram
+import find_peaks
+import audio_sampling
+
+
+log_spectro = create_logspectro(audio)
+peak_list = local_peaks(log_spectro, .77, 20)
+
