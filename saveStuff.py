45e64da263dd7b328a316d4af1eb09bb339590b5
Jessie <jessiexxcheung@gmail.com>
2019-07-11 16:49:09 -0400

    Jessie added imports

--- saveStuff.py
+++ saveStuff.py
@@ -1,7 +1,10 @@
 from audio_sampling import analog_to_digital, song_to_digital, turn_off_ticks
-
 import numpy as np
 import librosa
-import numpy as np
 import matplotlib.pyplot as plt
 from IPython.display import Audio
+import spectrogram
+import find_peaks
+%matplotlib notebook
+import matplotlib.mlab as mlab
+from microphone import record_audio
