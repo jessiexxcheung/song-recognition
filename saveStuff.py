b38eda617886d6803b14e5ee9d8e8d196df03287
Darshan <darsh797@gmail.com>
2019-07-11 23:22:23 -0400

    Cleaned up code, saved data with pickle, added comments

--- saveStuff.py
+++ saveStuff.py
@@ -3,17 +3,16 @@ import librosa
 from pathlib import Path
 import numpy as np
 from mutagen.mp3 import MP3
-sampling_rate = 44100
-bit_depth = 64
-dict = {}
-from pprint import pprint
 import pickle
+
+dict = {}  # Initializes an empty dictionary which the data will be added to.
+
 proj_root = Path(sys.path[0])  # sets the path of proj_root to song-recognition
 song_root = proj_root / r"songs"  # sets the path of song_root to song-recognition/songs
 
-
 files = sorted(song_root.glob('*.mp3'))  # Creates a list of all the song directories
 
+# Iterates through the files, takes samples on each of them, and adds each of them to the dictionary
 for item in range(len(files)):
     audio = MP3(files[item])
     samples, fs = librosa.load(files[item], sr=44100, mono=True, duration=audio.info.length)
@@ -21,5 +20,7 @@ for item in range(len(files)):
     samp *= (2 ** 15)
     samples = list(samp)
     dict[item] = samples
+
+# Saves the dictionary to pickle
 with open("dict.pkl",mode="wb") as songs_file:
     pickle.dump(dict, songs_file)
