a10e0609a7c52492c3b83170dca5275d60e8c951
Emily Y <emilygbs@yahoo.com>
2019-07-12 11:07:04 -0400

    fixed database issues

--- saveData.py
+++ saveData.py
@@ -138,7 +138,7 @@ def create_spec(audio):
     return np.log(S)
 
 
-def song_fingerprint(song_id, peaks, fan_out=15):
+def song_fingerprint(peaks, fan_out=15):
     '''
     Reads in the times and frequencies of the peaks and returns
     the unique fingerprint of the song based on the peaks.
@@ -163,28 +163,31 @@ def song_fingerprint(song_id, peaks, fan_out=15):
 
     '''
     d = {}
-    for p in range(len(peaks)):
-        if p > len(peaks)-fan_out:
-            compare_peaks = peaks[p+1:]
-        else:
-            compare_peaks = peaks[p+1:p+fan_out]
-        for c in range(len(compare_peaks)):
-            if (peaks[p][1], compare_peaks[c][1], compare_peaks[c][0]-peaks[p][0]) not in d:
-                d[(peaks[p][1], compare_peaks[c][1], compare_peaks[c][0]-peaks[p][0])] = [(song_id, peaks[p][0])]
+    for song_id in range(len(peaks)):
+        for p in range(len(peaks[song_id])):
+            if p > len(peaks)-fan_out:
+                compare_peaks = peaks[song_id][p+1:]
             else:
-                d[(peaks[p][1], compare_peaks[c][1], compare_peaks[c][0]-peaks[p][0])].append((song_id, peaks[p][0]))
-
-    with open(str(song_id) + ".pkl", mode="wb") as opened_file:
-        pickle.dump(d, opened_file)
+                compare_peaks = peaks[song_id][p+1:p+fan_out]
+            for c in range(len(compare_peaks)):
+                if (peaks[p][1], compare_peaks[c][1], compare_peaks[c][0]-peaks[p][0]) not in d:
+                    d[(peaks[p][1], compare_peaks[c][1], compare_peaks[c][0]-peaks[p][0])] = [(song_id, peaks[p][0])]
+                else:
+                    d[(peaks[p][1], compare_peaks[c][1], compare_peaks[c][0]-peaks[p][0])].append((song_id, peaks[p][0]))
 
     return d
 
 def main():
     digital_data = songSave()
+    pk_list = []
     for song in digital_data:
         spectrogram = create_spec(digital_data[song])
         peaks = local_peaks(spectrogram, 0.77, 15)
-        song_fingerprint(song, peaks)
+        pk_list.append(peaks)
+    song_fingerprint(pk_list)
+
+    with open("song_fingerprints.pkl", mode="wb") as opened_file:
+        pickle.dump(d, opened_file)
 
 if __name__ == "__main__":
     main()
