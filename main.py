321f9b61e3aa3a0c392c94dd230ab90e8289e313
Jessie <jessiexxcheung@gmail.com>
2019-07-12 10:19:33 -0400

    Edited main to include mic fingerprint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import audio_sampling, saveSongs, find_peaks, spectrogram, get_mic_input
+import audio_sampling, saveSongs, find_peaks, spectrogram, get_mic_input, fingerprints, pickle
 from microphone import record_audio
 import numpy as np
 
@@ -11,12 +11,9 @@ def main():
     mic_spec = get_mic_input.rec(10)     #Spectrogram of the mic input
 
     mic_peaks = find_peaks.local_peaks(mic_spec, .77, 20)  #Find peaks of the unknown samples
-
-    #Fingerprint the unknown samples
-
+    mic_fingerprint = fingerprints.sample_fingerprint(mic_peaks)   #Fingerprint the unknown samples
 
     #Check for matches of the unknown samples to the known sample dictionary
-
 
     #Return found or not found statement
 
